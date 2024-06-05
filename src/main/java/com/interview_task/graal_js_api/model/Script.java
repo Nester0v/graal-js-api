69a4fc442f58679fafd0a3a3e67c871bb3991e0d
Vladyslav Nesterov <vnesterov.promin@gmail.com>
2024-06-05 21:59:43 +0300

    added simple model to represent a JavaScript script with metadata

---
+++ src/main/java/com/interview_task/graal_js_api/model/Script.java
@@ -0,0 +1,69 @@
+package com.interview_task.graal_js_api.model;
+
+import java.time.LocalDateTime;
+public class Script {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public LocalDateTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public void setScheduledTime(LocalDateTime scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+
+    public LocalDateTime getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(LocalDateTime executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    private String code;
+    private String status;
+    private String output;
+    private String error;
+    private LocalDateTime scheduledTime;
+    private LocalDateTime executionTime;
+}
