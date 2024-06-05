3e92ab4cd8100bd3f7229ab94304ca326194c6de
Vladyslav Nesterov <vnesterov.promin@gmail.com>
2024-06-05 21:49:22 +0300

    project setup

---
+++ src/test/java/com/interview_task/graal_js_api/GraalJsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview_task.graal_js_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalJsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
